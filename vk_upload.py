5c5743ef3a2371003fa3e84bebc7f0808a7226d8
→«★BS NIK★»← <74881427+BSNIKYT@users.noreply.github.com>
2023-12-24 17:55:48 +0300

    Update vk_upload.py

--- vk_upload.py
+++ vk_upload.py
@@ -2,6 +2,16 @@ from modules.User import User
 import json
 user = User()
 def vk_uploader(file_name, dir_):
+    """
+        Загружает изображение на стену пользователя ВКонтакте и возвращает прямую ссылку на него.
+
+        Аргументы:
+            file_name (str): Имя файла для загрузки.
+            dir_ (str): Директория, в которой находится файл.
+
+        Возвращает:
+            str: Прямая ссылка на загруженное изображение.
+    """
     photo = user.picture_send(file_name, dir_)
     post_id = user.make_post(photo)['post_id']
 
@@ -11,4 +21,13 @@ def vk_uploader(file_name, dir_):
     return str(link)
 
 def vk_delete_post(post_id):
+    """
+        Удаляет пост пользователя ВКонтакте по указанному идентификатору.
+
+        Аргументы:
+            post_id (int): Идентификатор поста.
+
+        Возвращает:
+            None
+    """
     user.delete_post(post_id)
